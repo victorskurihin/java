79db78a52d9725904094ee57fa05d7b5875dda96
Victor Skurikhin <vs@rune.ru>
2018-03-04 10:55:34 +0300

    14-e домашнее задание курса "Разработчик Java" в Otus.ru

---
+++ src/test/java/ru/otus/l121/MainTest.java
@@ -0,0 +1,21 @@
+package ru.otus.l141;
+
+/**
+ * Created by VSkurikhin.
+ *
+ * Solution for L14.1
+ *
+ * To start:
+ * mvn clean package
+ *
+ */
+
+public class MainTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello");
+    }
+}
+
+/* vim: syntax=java:fileencoding=utf-8:fileformat=unix:tw=78:ts=4:sw=4:sts=4:et
+ */
+//EOF
